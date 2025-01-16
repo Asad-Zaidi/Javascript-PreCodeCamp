017fd2c01bebaf1dc9056f39b3d8cf4920448fe1
Syed Asad Jamil <syedasad1410@gmail.com>
2025-01-16 18:13:42 +0500

    JavaScript Variables

---
+++ Basic/variables.js
@@ -0,0 +1,25 @@
+/*
+
+Introduction to Variables
+
+What is a variable?
+
+A variable is a named storage location used to store data.
+
+x = 10
+
+x + 5 = 15
+
+*/
+
+// Rules of naming variables
+
+/* 
+
+1. Variable names must start with a letter, underscore (_), or dollar sign ($).
+2. Variable names can contain letters, numbers, underscores, and dollar signs.
+3. Variable names are case-sensitive (myVariable and MyVariable are different variables).
+4. Variable names cannot be JavaScript keywords or reserved words.
+
+
+*/
